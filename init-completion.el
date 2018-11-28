d7a7ad8f37e1b4bfd7b91b3c48efd1991d47e54b
Maciej Kalisiak <mkalisiak@gmail.com>
2018-11-28 12:05:57 -0500

    Trying out 'swiper' on C-s.

--- init-completion.el
+++ init-completion.el
@@ -37,6 +37,7 @@
 (setq ivy-count-format "(%d/%d) ")
 ;; (global-set-key (kbd "M-x") 'counsel-M-x)
 ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key "\C-s" 'swiper)
 
 ;; Config for 'helm'
 ;; 
