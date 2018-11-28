596e6716d6db41ebc358044638640ac91ca086ec
Maciej Kalisiak <mkalisiak@gmail.com>
2018-11-27 23:24:27 -0500

    Add configs for 'elfeed' and 'newsticker'.

--- init-misc.el
+++ init-misc.el
@@ -15,3 +15,11 @@
   (darkroom-mode)	; switch to darkroom-tentative-mode
 )
 (global-set-key [(control meta return)] `darkroom-mode)
+
+;; newsticker config
+(setq newsticker-url-list
+      '(("irreal" "http://irreal.org/blog/?feed=rss2" nil 3600 nil)
+	))
+
+;; elfeed config
+(setq shr-width 80) ; text width of content pane
