36dd6bbaf63186a4c7cc7275e9612be2cc422a70
Maciej Kalisiak <mkalisiak@gmail.com>
2018-11-28 23:48:48 -0500

    Switch to using helm (in lieu of IDO, ivy).

--- init-completion.el
+++ init-completion.el
@@ -12,33 +12,46 @@
 ;;
 ;; Interesting intro to IDO:
 ;;  http://www.masteringemacs.org/articles/2010/10/10/introduction-to-ido-mode/
-(ido-mode 1)
-(ido-grid-mode)
-(setq ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(setq ido-use-filename-at-point 'guess)
-(setq ido-create-new-buffer 'always)
+;;
+;; DISABLED because using helm.
+;; (ido-mode 1)
+;; (ido-grid-mode)
+;; (setq ido-everywhere t)
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-use-filename-at-point 'guess)
+;; (setq ido-create-new-buffer 'always)
 
 ;; NOTE: flex matching algo has some undesirable traits; see following
 ;; link for more info, and improved algo:
 ;;  http://scottfrazersblog.blogspot.com/2009/12/emacs-better-ido-flex-matching.html
 
 ;; Config for 'smex'; like 'ido', but for M-x commands.
-(require 'smex)
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; (require 'smex)
+;; (smex-initialize)
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 ;; Config for 'ivy'
 ;; 
-(require 'ivy)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-;; (global-set-key (kbd "M-x") 'counsel-M-x)
-;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key "\C-s" 'swiper)
+;; (require 'ivy)
+;; (ivy-mode 1)
+;; (setq ivy-use-virtual-buffers t)
+;; (setq ivy-count-format "(%d/%d) ")
+;; ;; (global-set-key (kbd "M-x") 'counsel-M-x)
+;; ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;; (global-set-key "\C-s" 'swiper)
 
 ;; Config for 'helm'
 ;; 
-;; (require 'helm-config)
+(require 'helm-config)
+(require 'helm-smex)
+(helm-mode)
+(global-set-key [remap execute-extended-command] #'helm-smex)
+(global-set-key (kbd "M-X") #'helm-smex-major-mode-commands)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+;(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+;(global-set-key (kbd "C-x C-b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;(global-set-key (kbd "M-B") 'helm-mini)
+(global-set-key (kbd "C-c h o") 'helm-occur)
