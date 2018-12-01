35cec30583ca0e891a0584f2e5eac00e58f4bb42
Maciej Kalisiak <mkalisiak@gmail.com>
2018-12-01 10:55:46 -0500

    Use 'fill-column-indicator.

--- init-looks.el
+++ init-looks.el
@@ -8,4 +8,14 @@
 (add-to-list 'default-frame-alist '(height . 45))
 (add-to-list 'default-frame-alist '(width . 120))
 
+;; Show boundary where words will wrap.
+(require 'fill-column-indicator)
+(fci-mode)
+(setq fill-column 80)
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (setq fill-column 78)
+	    (fci-mode 1)
+	    ))
+
 (setq inhibit-startup-message t)
